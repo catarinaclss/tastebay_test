0297b4532e6684c0a1266ae771e81113a4c7d940
Catarina <catarina.ufcg@gmail.com>
2017-04-13 01:06:41 -0300

    fixing routes

--- app.js
+++ app.js
@@ -11,19 +11,19 @@
         $routeProvider
             .when('/', {
                 controller: 'HomeController',
-                templateUrl: 'home/home.view.html',
+                templateUrl: 'home/home.html',
                 controllerAs: 'vm'
             })
 
             .when('/login', {
                 controller: 'LoginController',
-                templateUrl: 'login/login.view.html',
+                templateUrl: 'login/login.html',
                 controllerAs: 'vm'
             })
 
             .when('/register', {
                 controller: 'RegisterController',
-                templateUrl: 'register/register.view.html',
+                templateUrl: 'register/register.html',
                 controllerAs: 'vm'
             })
 
